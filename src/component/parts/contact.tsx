730ed6a91e32f6443a3cd02334bf3f28b2075513
Gild-shogi <ok2336.coffee@gmail.com>
2023-07-30 23:13:50 +0900

    wantedlyのロゴって正方形じゃないんだ。。。@

--- src/component/parts/contact.tsx
+++ src/component/parts/contact.tsx
@@ -8,7 +8,7 @@ import { HStack, Image, Link } from "@chakra-ui/react"
 function Contact(){
     return (
         <HStack spacing="0.1">
-            <Link href="https://www.wantedly.com/id/keito_oishi" isExternal><Image m="30px" src={wantedly} alt="wantedly Link" boxSize="40px"/></Link>
+            <Link href="https://www.wantedly.com/id/keito_oishi" isExternal><Image m="20px" src={wantedly} alt="wantedly Link" height="40px"/></Link>
             <Link href="https://github.com/Gild-shogi" isExternal><Image m="30px" src={github} alt="github Link" boxSize="40px"/></Link>
             <Link href="https://twitter.com/Gild_prog" isExternal><Image m="30px" src={twitter} alt="twitter Link" boxSize="40px"/></Link>
             <Link href="https://note.com/gild_coffee" isExternal><Image m="30px" src={note} alt="note Link" boxSize="40px"/></Link>
