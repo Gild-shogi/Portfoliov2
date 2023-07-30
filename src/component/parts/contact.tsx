0d455ab6b0f97236ad49f75ff663fc0d98d41f82
Gild-shogi <ok2336.coffee@gmail.com>
2023-07-30 23:09:20 +0900

    add wantedly profile

--- src/component/parts/contact.tsx
+++ src/component/parts/contact.tsx
@@ -1,12 +1,14 @@
-import github from "../../assets/GitHub-Mark-64px.png"
-import note from "../../assets/Noteicon.png"
-import twitter from "../../assets/TwitterB.png"
+import github from "../../assets/GitHub-Mark-64px.webp"
+import note from "../../assets/Noteicon.webp"
+import twitter from "../../assets/TwitterB.webp"
+import wantedly from "../../assets/Wantedly_Mark_LightBG.webp"
 
 import { HStack, Image, Link } from "@chakra-ui/react"
 
 function Contact(){
     return (
         <HStack spacing="0.1">
+            <Link href="https://www.wantedly.com/id/keito_oishi" isExternal><Image m="30px" src={wantedly} alt="wantedly Link" boxSize="40px"/></Link>
             <Link href="https://github.com/Gild-shogi" isExternal><Image m="30px" src={github} alt="github Link" boxSize="40px"/></Link>
             <Link href="https://twitter.com/Gild_prog" isExternal><Image m="30px" src={twitter} alt="twitter Link" boxSize="40px"/></Link>
             <Link href="https://note.com/gild_coffee" isExternal><Image m="30px" src={note} alt="note Link" boxSize="40px"/></Link>
