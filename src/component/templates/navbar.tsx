bb156c82586fcbe980c9115e3d8dae9527923de0
Gild-shogi <ok2336.coffee@gmail.com>
2024-03-15 13:05:05 +0900

    delete c4u js and add lazyload

--- src/component/templates/navbar.tsx
+++ src/component/templates/navbar.tsx
@@ -1,17 +1,19 @@
-import { Button, HStack, Image, Link, Spacer } from '@chakra-ui/react'
+import { Button, HStack, Image, Link, Spacer } from '@chakra-ui/react';
 
 function NavBar() {
-    return (
+  return (
+      <>
         <HStack as="header" top={0} zIndex="10" overflow="unset" position="sticky" bg="white">
         <Link href="#Skills"><Button>Skills</Button></Link>
         <Link href="#Products"><Button>Products</Button></Link>
         <Link href="#Publication"><Button>Publication</Button></Link>
         <Link href="#History"><Button>History</Button></Link>
         <Spacer />
-        <Link className="microtask repeat" href="//crowd4u.org">
+        <Link className="microtask repeat lazyload" href="//crowd4u.org">
           <Image src="//crowd4u.org/img/crowd4u_banner182x47.png"/>
         </Link>
         </HStack>
+      </>
     )
 }
 export default NavBar
